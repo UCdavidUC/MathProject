e21605b1d945e2f7fae92182bd63dce8dfd71d77
David Sanchez <divad.dad@gmail.com>
2016-01-18 23:10:52 -0600

    Initial commit

---
+++ src/mapreduce/MapReduce.java
@@ -0,0 +1,15 @@
+package mapreduce;
+
+/**
+ *
+ * @author Alejandra Martínez Cuevas
+ * @author David Sánchez Almanza
+ * @version 1.0
+ */
+public class MapReduce {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
